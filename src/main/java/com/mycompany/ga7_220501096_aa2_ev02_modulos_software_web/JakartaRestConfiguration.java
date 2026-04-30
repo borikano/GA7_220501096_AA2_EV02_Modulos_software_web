956d738a4e719154da6095ad4f178a043f84b466
lhecheverryo <luish.echeverryo@gmail.com>
2026-04-29 22:41:18 -0500

    chore: create Jakarta web project structure

---
+++ src/main/java/com/mycompany/ga7_220501096_aa2_ev02_modulos_software_web/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.mycompany.ga7_220501096_aa2_ev02_modulos_software_web;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
---
+++ src/main/java/com/mycompany/ga7_220501096_aa2_ev02_modulos_software_web/resources/JakartaEE10Resource.java
@@ -0,0 +1,20 @@
+package com.mycompany.ga7_220501096_aa2_ev02_modulos_software_web.resources;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("jakartaee10")
+public class JakartaEE10Resource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping Jakarta EE")
+                .build();
+    }
+}
